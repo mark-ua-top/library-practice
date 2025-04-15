33d432f237d247855287efcdcba393500558781a
mark-ua-top <126385729+mark-ua-top@users.noreply.github.com>
2025-04-15 19:32:04 +0000

    Deploying to gh-pages from @ mark-ua-top/library-practice@a4cab34cd9696fa8e98ae687e2a45389ae15b526 🚀

---
+++ library-practice.1bbb2ba3.js
@@ -0,0 +1,2 @@
+const o=Number(prompt("введіть число")),l=Number(prompt("введіть число"));console.log(o+l),console.log(["Olga","Tima","Vanya","I"]),console.log(o-l),console.log("test"),console.log(o/l);
+//# sourceMappingURL=library-practice.1bbb2ba3.js.map
--- library-practice.9022f427.js
+++
@@ -1,2 +0,0 @@
-console.log("hello world");
-//# sourceMappingURL=library-practice.9022f427.js.map
